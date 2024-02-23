28095ab2b8638016e6a6c1fabb9a41052c63013d
Matthias Keller <matthias.b.keller@bluewin.ch>
2024-02-23 20:52:33 +0100

    Updated demo-app + put a link to demo-app on library-homepage

--- mk-magic-alerts-demo/src/app/app.component.ts
+++ mk-magic-alerts-demo/src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AlertsService } from '@mk-magic-alerts';
 import packageJson from '../../package.json';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -9,17 +10,68 @@ import packageJson from '../../package.json';
 })
 export class AppComponent implements OnInit {
   title = 'mk-magic-alerts-demo';
+  alertsForm: FormGroup;
 
   constructor(private msgSvc: AlertsService) {
-
+    this.alertsForm = new FormGroup({
+      successMessage: new FormControl('', Validators.required),
+      successDuration: new FormControl(1, [Validators.required, Validators.min(1)]),
+      errorMessage: new FormControl('', Validators.required),
+      errorDuration: new FormControl(1, [Validators.required, Validators.min(1)]),
+      infoMessage: new FormControl('', Validators.required),
+      infoDuration: new FormControl(1, [Validators.required, Validators.min(1)]),
+      warningMessage: new FormControl('', Validators.required),
+      warningDuration: new FormControl(1, [Validators.required, Validators.min(1)])
+    });
   }
 	ngOnInit(): void {
+    
     console.log(`Frontend Version: v${packageJson?.version}`);
 
-		this.msgSvc.showSuccess('TEST', 6_000);
-		this.msgSvc.showError('This is a test-message with a very long text. This is a test-message with a very long text. ');
-		this.msgSvc.showInfo('INFO');
-		this.msgSvc.showWarning('Warning');
+		this.displaySuccess('Success', 5);
+		this.displayError('Error', 6);
+		this.displayInfo('Info', 7);
+		this.displayWarning('Warning', 8);
 	}
+
+  submitSuccess() {
+    const { successMessage, successDuration } = this.alertsForm.value;
+    this.displaySuccess(successMessage, successDuration);
+  }
+
+  submitError() {
+    const { errorMessage, errorDuration } = this.alertsForm.value;
+    this.displayError(errorMessage, errorDuration);
+  }
+
+  submitInfo() {
+    const { infoMessage, infoDuration } = this.alertsForm.value;
+    this.displayInfo(infoMessage, infoDuration);
+  }
+
+  submitWarning() {
+    const { warningMessage, warningDuration } = this.alertsForm.value;
+    this.displayWarning(warningMessage, warningDuration);
+  }
+
+  private displaySuccess(msg: string, durationInSec: number) {
+    const durationInMillis = (durationInSec ?? 1) * 1000;
+    this.msgSvc.showSuccess(msg, durationInMillis);
+  }
+
+  private displayError(msg: string, durationInSec: number) {
+    const durationInMillis = (durationInSec ?? 1) * 1000;
+    this.msgSvc.showError(msg, durationInMillis);
+  }
+
+  private displayInfo(msg: string, durationInSec: number) {
+    const durationInMillis = (durationInSec ?? 1) * 1000;
+    this.msgSvc.showInfo(msg, durationInMillis);
+  }
+
+  private displayWarning(msg: string, durationInSec: number) {
+    const durationInMillis = (durationInSec ?? 1) * 1000;
+    this.msgSvc.showWarning(msg, durationInMillis);
+  }
 
 }
--- mk-magic-alerts-demo/src/app/app.module.ts
+++ mk-magic-alerts-demo/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { MkMagicAlertsModule } from '@mk-magic-alerts';
 
+import { ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -12,6 +13,7 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ReactiveFormsModule ,
     AppRoutingModule,
     MkMagicAlertsModule,
   ],
