56e833876c9f600fd310c239da25dfd6691f1ea5
Matthias Keller <matthias.b.keller@bluewin.ch>
2024-05-25 23:47:54 +0200

    Upgraded configs

--- mk-magic-alerts-demo/copyIndexTo404.js
+++ mk-magic-alerts-demo/copyIndexTo404.js
@@ -1,8 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
-const srcPath = path.join(__dirname, 'dist', 'mk-magic-alerts-demo', 'index.html');
-const destPath = path.join(__dirname, 'dist', 'mk-magic-alerts-demo', '404.html');
+const srcPath = path.join(__dirname, 'dist', 'mk-magic-alerts-demo', 'browser', 'index.html');
+const destPath = path.join(__dirname, 'dist', 'mk-magic-alerts-demo', 'browser', '404.html');
 
 fs.copyFile(srcPath, destPath, (err) => {
   if (err) throw err;
