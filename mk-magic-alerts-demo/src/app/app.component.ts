74bd64a32e7000a99e58e55b457bec107f1261e3
Matthias Keller <matthias.b.keller@bluewin.ch>
2025-08-22 22:38:35 +0200

    Updated demo-app

--- mk-magic-alerts-demo/src/app/app.component.ts
+++ mk-magic-alerts-demo/src/app/app.component.ts
@@ -16,63 +16,63 @@ export class AppComponent implements OnInit {
   constructor(private msgSvc: AlertsService) {
     this.alertsForm = new FormGroup({
       successAlert: new FormControl('Success!', Validators.required),
-      successDuration: new FormControl(1, [Validators.required, Validators.min(1)]),
+      successDuration: new FormControl(3, [Validators.required, Validators.min(1)]),
       errorAlert: new FormControl('Error!', Validators.required),
-      errorDuration: new FormControl(1, [Validators.required, Validators.min(1)]),
+      errorDuration: new FormControl(3, [Validators.required, Validators.min(1)]),
       infoAlert: new FormControl('Info!', Validators.required),
-      infoDuration: new FormControl(1, [Validators.required, Validators.min(1)]),
+      infoDuration: new FormControl(3, [Validators.required, Validators.min(1)]),
       warningAlert: new FormControl('Warning!', Validators.required),
-      warningDuration: new FormControl(1, [Validators.required, Validators.min(1)])
+      warningDuration: new FormControl(3, [Validators.required, Validators.min(1)])
     });
   }
 
 	ngOnInit(): void {
-    console.log(`Frontend Version: v${packageJson?.version}`);
+		console.log(`Frontend Version: v${packageJson?.version}`);
 
 		this.displaySuccess('Success-Alert', 5);
-    setTimeout(() => this.displayError('Error-Alert', 5), 500);
-    setTimeout(() => this.displayInfo('Info-Alert', 5), 1000);
-    setTimeout(() => this.displayWarning('Warning-Alert', 5), 1500);
+		setTimeout(() => this.displayError('Error-Alert', 5), 500);
+		setTimeout(() => this.displayInfo('Info-Alert', 5), 1000);
+		setTimeout(() => this.displayWarning('Warning-Alert', 5), 1500);
 	}
 
-  submitSuccess() {
-    const { successAlert, successDuration } = this.alertsForm.value;
-    this.displaySuccess(successAlert, successDuration);
-  }
+	submitSuccess() {
+		const { successAlert, successDuration } = this.alertsForm.value;
+		this.displaySuccess(successAlert, successDuration);
+	}
 
-  submitError() {
-    const { errorAlert, errorDuration } = this.alertsForm.value;
-    this.displayError(errorAlert, errorDuration);
-  }
+	submitError() {
+		const { errorAlert, errorDuration } = this.alertsForm.value;
+		this.displayError(errorAlert, errorDuration);
+	}
 
-  submitInfo() {
-    const { infoAlert, infoDuration } = this.alertsForm.value;
-    this.displayInfo(infoAlert, infoDuration);
-  }
+	submitInfo() {
+		const { infoAlert, infoDuration } = this.alertsForm.value;
+		this.displayInfo(infoAlert, infoDuration);
+	}
 
-  submitWarning() {
-    const { warningAlert, warningDuration } = this.alertsForm.value;
-    this.displayWarning(warningAlert, warningDuration);
-  }
+	submitWarning() {
+		const { warningAlert, warningDuration } = this.alertsForm.value;
+		this.displayWarning(warningAlert, warningDuration);
+	}
 
-  private displaySuccess(msg: string, durationInSec: number) {
-    const durationInMillis = (durationInSec ?? 1) * 1000;
-    this.msgSvc.showSuccess(msg, durationInMillis);
-  }
+	private displaySuccess(msg: string, durationInSec: number) {
+		const durationInMillis = (durationInSec ?? 1) * 1000;
+		this.msgSvc.showSuccess(msg, durationInMillis);
+	}
 
-  private displayError(msg: string, durationInSec: number) {
-    const durationInMillis = (durationInSec ?? 1) * 1000;
-    this.msgSvc.showError(msg, durationInMillis);
-  }
+	private displayError(msg: string, durationInSec: number) {
+		const durationInMillis = (durationInSec ?? 1) * 1000;
+		this.msgSvc.showError(msg, durationInMillis);
+	}
 
-  private displayInfo(msg: string, durationInSec: number) {
-    const durationInMillis = (durationInSec ?? 1) * 1000;
-    this.msgSvc.showInfo(msg, durationInMillis);
-  }
+	private displayInfo(msg: string, durationInSec: number) {
+		const durationInMillis = (durationInSec ?? 1) * 1000;
+		this.msgSvc.showInfo(msg, durationInMillis);
+	}
 
-  private displayWarning(msg: string, durationInSec: number) {
-    const durationInMillis = (durationInSec ?? 1) * 1000;
-    this.msgSvc.showWarning(msg, durationInMillis);
-  }
+	private displayWarning(msg: string, durationInSec: number) {
+		const durationInMillis = (durationInSec ?? 1) * 1000;
+		this.msgSvc.showWarning(msg, durationInMillis);
+	}
 
 }
